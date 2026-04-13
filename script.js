f8e7782af1e539ea6bce7b7020b37e1f27f35d4b
odushey <123371245+odushey@users.noreply.github.com>
2026-04-13 19:17:53 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const puppeteer = require('puppeteer');
+(async () => {
+  const browser = await puppeteer.launch({headless: true});
+  const page = await browser.newPage();
+  await page.setViewport({width: 1400, height: 900});
+  await page.goto('http://127.0.0.1:8080/index.html');
+  
+  const rects = await page.evaluate(() => {
+    return {
+      bodyWidth: document.body.clientWidth,
+      pricingContainerRect: document.querySelector('#pricing .container').getBoundingClientRect(),
+      pricingCardsRect: document.querySelector('.pricing-cards').getBoundingClientRect(),
+      headlineRect: document.querySelector('#pricing .section-headline').getBoundingClientRect(),
+      counterContainerRect: document.querySelector('#counter-bar .container').getBoundingClientRect(),
+      heroContainerRect: document.querySelector('.hero-section .container').getBoundingClientRect()
+    };
+  });
+  console.log(JSON.stringify(rects, null, 2));
+  
+  await browser.close();
+})();
